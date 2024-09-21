b5089668d5ba52cfbcecf4c7647e1a283366a662
vemulamuralisaipraharsha <vemula.praharsha21b@iiitg.ac.in>
2024-09-21 17:11:45 +0530

    2nd commit bug fix

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 4000;
 
 app.get("/api/v1/posts", async (req: any, res: any) => {
-    const {flag, data} = req.params;
+    const {flag, data} = req.query;
+    console.log(flag,data);
     try {
         if(flag == "CREATE") {
             // const createResponse = await axios.get('/api/v1/posts/create', {
