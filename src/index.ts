4695370fdb27b373475fe650ec4f7ff3f3b9c067
RahulYavvari <rahulyavvari04@gmail.com>
2024-10-01 15:55:56 +0530

    Create and Read Update

--- src/index.ts
+++ src/index.ts
@@ -12,29 +12,30 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 4000;
 
 app.get("/api/v1/posts", async (req: any, res: any) => {
-    const {flag, data} = req.query;
+    let { flag, data, tag, nocache } = req.query;
     try {
-        if(flag == "CREATE") {
+        if (flag == "CREATE" && data != null) {
             console.log(data);
-            const createResponse = await axios.get(`${process.env.ZSOURCE_CREATE_SERVER_URI}/api/v1/posts/create?data=${data}`); 
+            const createResponse = await axios.get(`${process.env.ZSOURCE_CREATE_SERVER_URI}/api/v1/posts/create?data=${data}`);
             res.json(createResponse.data);
-        } else if(flag == "READ") {
-            const readResponse = await axios.get(`${process.env.ZSOURCE_READ_SERVER_URI}/api/v1/posts/read`, {
-                data
-            });
-            res.json(JSON.stringify({readResponse}));
+        } else if (flag == "READ" && tag != null) {
+            if (!nocache) {
+                nocache = false;
+            }
+            const readResponse = await axios.get(`${process.env.ZSOURCE_READ_SERVER_URI}/api/v1/posts/read?tag=${tag}&nocache=${nocache}`);
+            res.json(JSON.stringify({ readResponse }));
         }
 
     } catch (error) {
-        res.json(JSON.stringify({err: "1024 Error Occured"}));
+        res.json(JSON.stringify({ err: "1024 Error Occured" }));
     }
 })
 
-app.get("/health", (req: any,res: any)=>{
-        res.send({data:"data from health checks"});
-        console.log("printing from health route");
+app.get("/health", (req: any, res: any) => {
+    res.send({ data: "data from health checks" });
+    console.log("printing from health route");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,  "10.3.0.210", () => {
     console.log(`[LOG] API server running on port ${PORT}`);
 });
